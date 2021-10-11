4c02379a0489a530b419e8bb9a904d4d376d2a3d
pickatwu <75255265+pickatwu@users.noreply.github.com>
2021-10-12 00:54:37 +0800

    Update dns.py

--- dns.py
+++ dns.py
@@ -6,6 +6,17 @@ ip ="'pickatwu.github.io'
 sock = socket.socket(socket.AF_INET, socket,SOCK_DRAM)
 sock.bind((ip, port))
 
+def buildresponse(data):
+
+  TransactionID = data[0:2]
+  
+  for byte in TransactionID:
+    print(byte)
+  
 while 1;
-data, addr = sock.recvfrom()
-print()
+data, addr = sock.recvfrom(512)
+print(Data)
+sock.sendto(b'Load',addr)
+
+r =buildresponse(data)
+sock.sendto(r ,addr)
