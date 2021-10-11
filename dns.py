e7287a0d175bc140c42acb794893f3009bb8d1cd
pickatwu <75255265+pickatwu@users.noreply.github.com>
2021-10-12 00:39:26 +0800

    Update dns.py

--- dns.py
+++ dns.py
@@ -1,1 +1,11 @@
 
+import socket
+port 53
+ip ="'pickatwu.github.io'
+
+sock = socket.socket(socket.AF_INET, socket,SOCK_DRAM)
+sock.bind((ip, port))
+
+while 1;
+data, addr = sock.recvfrom()
+print()
